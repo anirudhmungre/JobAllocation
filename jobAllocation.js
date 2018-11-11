41a003f47206f86303ebc2a6509b136a0307345e
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-11 16:40:11 -0500

    initially assign all minimums

--- jobAllocation.js
+++ jobAllocation.js
@@ -6,9 +6,36 @@ function getEffectiveness(fName) {
         arr.push(lines[i].split(' '))
     }
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i].map(function (x) {return parseInt(x)})
+        arr[i] = arr[i].map(function (x) { return parseInt(x) })
     }
     return arr
 }
 
-console.log(getEffectiveness("input.txt"))
+function getAllIndexes(arr, val) {
+    let indexes = [], i
+    // console.log(arr + "     " + val)
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] == val)
+            indexes.push(i)
+    return indexes
+}
+
+function checkInitAssignment(effectiveness) {
+    let temp, index, initAssign, min
+    temp = effectiveness
+    initAssign = new Array(temp.length)
+    initAssign.fill([])
+    for (let i = 0; i < temp.length; i++) {
+        min = Math.min.apply(null, temp[i])
+        console.log(temp[i] + "      " + min)
+        index = temp[i].indexOf(min)
+        initAssign[i] = getAllIndexes(temp[i], min)
+    }
+    if (initAssign.indexOf([]) > -1) { return false }
+    else { return initAssign }
+}
+
+let effectiveness, initAssign
+effectiveness = getEffectiveness("input.txt")
+initAssign = checkInitAssignment(effectiveness)
+console.log(initAssign)
